d651dceac23fb64e9dddd2b6594d7267330599ea
Sourav Sharma <souravgopal25@gmail.com>
2023-05-06 17:20:32 +0530

    Unary Transmission Working!

--- grpc_poc/grpc_client/src/main/java/com/example/service/BookAuthorClientService.java
+++ grpc_poc/grpc_client/src/main/java/com/example/service/BookAuthorClientService.java
@@ -4,6 +4,8 @@ import com.example.Author;
 import com.example.AuthorId;
 import com.example.BookAuthorServiceGrpc;
 import com.google.protobuf.Descriptors;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +13,17 @@ import java.util.Map;
 
 @Service
 public class BookAuthorClientService {
-    @GrpcClient("grpc-service")
-    BookAuthorServiceGrpc.BookAuthorServiceBlockingStub synchronousClient;
+//    @GrpcClient("grpc-service")
+//    BookAuthorServiceGrpc.BookAuthorServiceBlockingStub synchronousClient;
 
 //    @GrpcClient("grpc-service")
 //    BookAuthorServiceGrpc.BookAuthorServiceStub asynchronousClient;
 
     public Map<Descriptors.FieldDescriptor,Object> getAuthor(int authorId){
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9000)
+                .usePlaintext()
+                .build();
+        BookAuthorServiceGrpc.BookAuthorServiceBlockingStub synchronousClient=BookAuthorServiceGrpc.newBlockingStub(channel);
         AuthorId request=AuthorId.newBuilder().setAuthorId(authorId).build();
         Author response=synchronousClient.getAuthorDetails(request);
         return response.getAllFields();
