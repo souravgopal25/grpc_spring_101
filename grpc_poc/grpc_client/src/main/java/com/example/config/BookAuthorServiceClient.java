45a5d451eca856768215d12f5401e0e6b721471b
Sourav Sharma <souravgopal25@gmail.com>
2023-05-08 16:39:24 +0530

    Configurable using applications.yml

--- grpc_poc/grpc_client/src/main/java/com/example/config/BookAuthorServiceClient.java
+++ grpc_poc/grpc_client/src/main/java/com/example/config/BookAuthorServiceClient.java
@@ -3,14 +3,20 @@ package com.example.config;
 import com.example.BookAuthorServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class BookAuthorServiceClient {
+    @Value("book-author-service.address")
+    String address;
+
+
     private ManagedChannel getManagedChannel() {
         return ManagedChannelBuilder
-                .forAddress("localhost", 9000)
+                .forAddress(address, 9000)
                 .usePlaintext()
                 .build();
     }
